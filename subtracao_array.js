7a5853b798d6bde16c66e3efed321232cc97eeb8
KAYLANE MAFRA STRIZZI <kaylane_strizzi@estudante.sesisenai.org.br>
2024-03-06 15:33:59 -0300

    soma de arrays

---
+++ subtracao_array.js
@@ -0,0 +1,23 @@
+const readlineSync = require('readline-sync')
+
+let array1 = [1, 2, 3, 4, 5]
+let array2 = []
+let array3 = []
+
+let valor = 0
+
+function ler(){
+
+
+}
+
+
+function subtracao(){
+
+}
+
+function imprimir(){
+
+}
+
+console.log("A subtração entre os números é = " , subtracao())
